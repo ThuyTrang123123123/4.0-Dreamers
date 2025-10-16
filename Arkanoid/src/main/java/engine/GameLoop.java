f6e34717a2f9dce37ed071cfe45c73f9855e636c
Nhi <yennhi.vt06@gmail.com>
2025-10-16 18:24:31 +0700

    update Paddle

--- Arkanoid/src/main/java/engine/GameLoop.java
+++ Arkanoid/src/main/java/engine/GameLoop.java
@@ -1,4 +1,24 @@
+// engin/GameLoop.java
 package engine;
 
-public class GameLoop {
+import javafx.animation.AnimationTimer;
+import core.Game;
+
+public class GameLoop extends AnimationTimer {
+    private Game game;
+    private long lastTime = 0;
+
+    public GameLoop(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void handle(long now) {
+        if (lastTime > 0) {
+            double deltaTime = (now - lastTime) / 1_000_000_000.0;
+            game.update(deltaTime);
+            game.render();
+        }
+        lastTime = now;
+    }
 }
