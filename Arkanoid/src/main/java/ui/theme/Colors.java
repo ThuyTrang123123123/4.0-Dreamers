d40c7654774989f9603371f535e2291b1053d990
Thuỳ Tranng <thuytranguete@gmail.com>
2025-11-08 00:23:16 +0700

    đổi màu button

--- Arkanoid/src/main/java/ui/theme/Colors.java
+++ Arkanoid/src/main/java/ui/theme/Colors.java
@@ -7,7 +7,7 @@ public class Colors {
     public static final Color BACKGROUND = Color.web("#FFF8F6"); // Hồng nhạt
     public static final Color TEXT = Color.web("#000000"); //Đen
     public static final Color HUD_BG = Color.web("#FFFF88"); // Đen xám
-    public static final Color BUTTON = Color.web("#B3EF9E"); //
+    public static final Color BUTTON = Color.web("#B3EF9E"); //xanh ngọc
     public static final Color SECONDARY = Color.web("#FFB6A4");
     public static final Color BRICKS = Color.OLIVE;
 //    public static final Color BUTTON = Color.web("#FFB6A4");
